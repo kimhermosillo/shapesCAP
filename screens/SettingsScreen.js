7d5c150dc40a622e6bbefc0d11f470f48650dbb3
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-09-22 17:31:33 -0600

    every compnent works along with select date for food journal

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { StyleSheet, View, AppRegistry, TextInput, ScrollView } from 'react-native'
 import { ExpoConfigView } from '@expo/samples'
-import { Container, Header, Content, Button, Text } from 'native-base'
+import { Container, Header, Content, Button, Text, DatePicker } from 'native-base'
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
     title: 'Food Journal',
   };
+
   constructor(props) {
     super(props)
     this.state = { text: ' Log your food here ' }
@@ -19,6 +20,25 @@ export default class SettingsScreen extends React.Component {
           <Text> Keeping track of your meals is important to be able to keep track of your calorie goals!
           Make sure to remember serving size, calorie, carb, and protein goals for each meal.
           </Text>
+          {/* DATE PICKER */}
+          <Content>
+            <DatePicker
+              defaultDate={new Date(2018, 4, 4)}
+              minimumDate={new Date(2018, 1, 1)}
+              maximumDate={new Date(2018, 12, 31)}
+              locale={'en'}
+              timeZoneOffsetInMinutes={undefined}
+              modalTransparent={false}
+              placeHolderText="Select Date"
+              textStyle={{ color: 'green '}}
+              placeHolderTextStyle={{ color: '#d3d3d3' }}
+              onDateChange={ this.setDate }
+            />
+            <Text>
+              Date: {this.state.chosenDate}
+            </Text>
+          </Content>
+          {/* END OF DATE PICKER */}
           <Text style={styles.textJournal}>What did you eat today?</Text>
           <TextInput style={
             {height: 90, 
@@ -32,8 +52,8 @@ export default class SettingsScreen extends React.Component {
           onChangeText={(text) => this.setState({text})}
           value={this.state.text}
           />
-          <Button block light>
-            <Text>Light</Text>
+          <Button block dark>
+            <Text>Submit journal</Text>
           </Button>
           <Text style={styles.textJournal}>
             Past entries:
